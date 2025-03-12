3f66a8371a1e775e259a7c81b5d94e5d311b4fb9
18101224 <alswo01287@naver.com>
2025-03-12 15:25:21 +0900

    dataset init

---
+++ dataset/init_dataset.py
@@ -0,0 +1,12 @@
+from . import FER
+from argparse import Namespace
+
+if __name__ == '__main__':
+    args = {
+        'dataset_path':'../data/AffectNet7',
+        'world_size':1,
+
+    }
+    args = Namespace(**args)
+    dataset = FER(args,ckpt_path='checkpoint/quality',train=True)
+    dataset = FER(args,ckpt_path='checkpoint/quality',train=False)
