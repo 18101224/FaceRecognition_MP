da8b3d7adab86fac95cd1b62f579ec364417934e
18101224 <alswo01287@naver.com>
2025-03-12 14:54:49 +0900

    ww

--- shells/s_adaptive.sh
+++ shells/s_adaptive.sh
@@ -1,10 +1,10 @@
 CUDA_VISIBLE_DEVICES=0,1 /pscratch/sd/s/sgkim/hcir/condaenvs/hcir/bin/torchrun --nproc_per_node=2 --rdzv-backend=c10d --rdzv-endpoint=localhost:29500 --nnodes=1 pretrain.py --world_size=2 \
---learning_rate=0.0003 --batch_size=256 \
+--learning_rate=0.0005 --batch_size=256 \
 --dataset_name=AffectNet --dataset_path=../data/AffectNet7 \
 --wandb_token=../wandb.txt --kp_rpe_cfg_path=checkpoint/adaface_vit_base_kprpe_webface12m \
 --n_epochs=40  \
 --LDAM=True --ldam_weight=0.1 \
 --cos_margin_loss=True --margin=0.3 \
---name only_margin_high --quality_model_path=checkpoint/quality \
+--name only_margin_high5e4 --quality_model_path=checkpoint/quality \
 --instance_adaloss_ckpt=checkpoint/vanilla12m \
 --proportion_alpha=0
