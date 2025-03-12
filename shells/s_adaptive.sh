393798d155098219cc6dad482626a0620e426938
18101224 <alswo01287@naver.com>
2025-03-12 14:53:34 +0900

    ww

--- shells/s_adaptive.sh
+++ shells/s_adaptive.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1 /pscratch/sd/s/sgkim/hcir/condaenvs/cv2/bin/torchrun --nproc_per_node=2 --rdzv-backend=c10d --rdzv-endpoint=localhost:29500 --nnodes=1 pretrain.py --world_size=2 \
+CUDA_VISIBLE_DEVICES=0,1 /pscratch/sd/s/sgkim/hcir/condaenvs/hcir/bin/torchrun --nproc_per_node=2 --rdzv-backend=c10d --rdzv-endpoint=localhost:29500 --nnodes=1 pretrain.py --world_size=2 \
 --learning_rate=0.0003 --batch_size=256 \
 --dataset_name=AffectNet --dataset_path=../data/AffectNet7 \
 --wandb_token=../wandb.txt --kp_rpe_cfg_path=checkpoint/adaface_vit_base_kprpe_webface12m \
