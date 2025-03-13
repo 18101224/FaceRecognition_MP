4a464501a08883e8dd940014372954e854c664de
18101224 <alswo01287@naver.com>
2025-03-13 20:27:22 +0900

    7

---
+++ shells/7_adaptive.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=2 python3 pretrain.py --world_size=1 \
+--learning_rate=0.000003 --batch_size=128 \
+--dataset_name=AffectNet --dataset_path=../data/AffectNet7 \
+--wandb_token=../wandb.txt --kp_rpe_cfg_path=checkpoint/adaface_vit_base_kprpe_webface12m \
+--n_epochs=200  \
+--name only_margin_low1e-6 --quality_model_path=checkpoint/quality \
+--instance_adaloss_ckpt=checkpoint/vanilla12m \
+--proportion_alpha=0 &
