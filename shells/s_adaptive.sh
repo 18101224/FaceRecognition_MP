674b03a5858e527b171307cd3dec4513153b2d22
18101224 <alswo01287@naver.com>
2025-03-13 19:34:30 +0900

    else

--- shells/s_adaptive.sh
+++ shells/s_adaptive.sh
@@ -12,7 +12,7 @@ CUDA_VISIBLE_DEVICES=2 python3 pretrain.py --world_size=1 \
 --dataset_name=AffectNet --dataset_path=../data/AffectNet7 \
 --wandb_token=../wandb.txt --kp_rpe_cfg_path=checkpoint/adaface_vit_base_kprpe_webface12m \
 --n_epochs=200  \
---name only_margin_high5e4 --quality_model_path=checkpoint/quality \
+--name only_margin_low1e-6 --quality_model_path=checkpoint/quality \
 --instance_adaloss_ckpt=checkpoint/vanilla12m \
 --proportion_alpha=0 &
 
@@ -21,6 +21,6 @@ CUDA_VISIBLE_DEVICES=3 python3 pretrain.py --world_size=1 \
 --dataset_name=AffectNet --dataset_path=../data/AffectNet7 \
 --wandb_token=../wandb.txt --kp_rpe_cfg_path=checkpoint/adaface_vit_base_kprpe_webface12m \
 --n_epochs=200  \
---name only_margin_high5e4 --quality_model_path=checkpoint/quality \
+--name only_margin_low5e-6 --quality_model_path=checkpoint/quality \
 --instance_adaloss_ckpt=checkpoint/vanilla12m \
 --proportion_alpha=0
