d0abe3a6d48c48b7f967bc3cc7c2d557a26e2dbf
18101224 <alswo01287@naver.com>
2025-08-21 11:11:06 +0900

    arg added

--- shells/1_finetune.sh
+++ shells/1_finetune.sh
@@ -11,7 +11,7 @@ make_cmd () {
         --learning_rate="$LR" --batch_size=256 --n_epochs=200 --weight_decay=5e-4 \
         --cos=True --momentum=0.9 --world_size=1 --wandb_token=../wandb.txt \
         --model_type=resnet32 --dataset_name=cifar10 --imb_type=exp --imb_factor=0.01 \
-        --dataset_path=../data --aug=True --cutout=True --use_wandb=True  --feature_branch=True $EXTRA
+        --dataset_path=../data --aug=True --cutout=True --use_wandb=True  --feature_branch=True --feature_module=deepcomplex_3 $EXTRA
 }
 
 
