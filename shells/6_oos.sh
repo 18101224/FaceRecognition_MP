f5a63749b663136584c1dab3c541bd6c26772cee
18101224 <alswo01287@naver.com>
2025-08-22 13:39:38 +0900

    server6 shell

--- shells/6_oos.sh
+++ shells/6_oos.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # run_all_cifar100.sh
 
-
 LEARNING_RATES=(0.15 0.1 0.07 0.05)
 
 make_cmd () {
   local DEVICE=$1
   local LR=$2
   local EXTRA=$3        # loss·weight·스케줄 인자 묶음
-  "$DEVICE "python3 train_imbalanced.py \
+  CUDA_VISIBLE_DEVICES="$DEVICE" python3 train_imbalanced.py \
         --learning_rate="$LR" --batch_size=256 --n_epochs=200 --weight_decay=5e-4 \
         --cos=True --momentum=0.9 --world_size=1 --wandb_token=../wandb.txt \
         --model_type=resnet32 --dataset_name=cifar10 --imb_type=exp --imb_factor=0.01 \
@@ -18,12 +17,12 @@ make_cmd () {
 
 for LR in "${LEARNING_RATES[@]}"; do
 
-  make_cmd "CUDA_VISIBLE_DEVICES=0" "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.1  --cosine_scaling=32  \
+  make_cmd "0" "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.1  --cosine_scaling=32  \
                           --temperature=0.1 --scheduler=warmup" &  
-  make_cmd "CUDA_VISIBLE_DEVICES=1" "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.5  --cosine_scaling=32  \
+  make_cmd "1" "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.5  --cosine_scaling=32  \
                           --temperature=0.1 --scheduler=warmup" &  
                           
-  make_cmd "CUDA_VISIBLE_DEVICES=2" "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=1  --cosine_scaling=32  \
+  make_cmd "2" "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=1  --cosine_scaling=32  \
                           --temperature=0.1 --scheduler=warmup" &  
 
 
