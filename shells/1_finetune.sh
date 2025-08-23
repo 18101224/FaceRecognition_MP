810ffb99eeb1493e5b19fde2067f9ad095a1c976
18101224 <alswo01287@naver.com>
2025-08-23 19:32:54 +0900

    shell

--- shells/1_finetune.sh
+++ shells/1_finetune.sh
@@ -11,13 +11,13 @@ make_cmd () {
         --learning_rate="$LR" --batch_size=256 --n_epochs=200 --weight_decay=5e-4 \
         --cos=True --momentum=0.9 --world_size=1 --wandb_token=../wandb.txt \
         --model_type=e2_resnet32 --dataset_name=cifar100 --imb_type=exp --imb_factor=0.01 \
-        --dataset_path=../data --aug=True --cutout=True --use_wandb=True  --feature_branch=True --use_mean=True --regular_simplex=True --feature_module=deepcomplex_3 $EXTRA
+        --dataset_path=../data --aug=True --cutout=True --use_wandb=True  --feature_branch=True  $EXTRA
 }
 
 
 for LR in "${LEARNING_RATES[@]}"; do
 
-  make_cmd "$LR" "--loss=CE --ce_weight=2 --cl_weight=0.6  --cosine_scaling=32  \
+  make_cmd "$LR" "--loss=BCL --ce_weight=2 --cl_weight=0.6  --cosine_scaling=32  \
                           --temperature=0.1 --scheduler=warmup" 
 
   # make_cmd "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.3  --cosine_scaling=32  \
