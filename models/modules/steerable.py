b6ebca011b3bfe759ab566afed15a855ba0ddfce
18101224 <alswo01287@naver.com>
2025-08-23 15:02:25 +0900

    steerable cnn editted

--- models/modules/steerable.py
+++ models/modules/steerable.py
@@ -75,8 +75,7 @@ class E2ResNet32(nn.Module):
         self.layer3 = self._make_layer(current_type, 64, 5, stride=1)  # 64 regular (dim=256)
         current_type = self.layer3[-1].out_type  # 최종 256
         
-        # Final linear: from 256 dim to num_classes
-        self.linear = nn.Linear(256, num_classes)  # 64 multiplicity * 4 = 256
+
 
     def _make_layer(self, in_type: FieldType, multiplicity: int, num_blocks: int, stride: int):
         out_type = FieldType(self.r2_act, multiplicity * [self.r2_act.regular_repr])
@@ -103,7 +102,7 @@ class E2ResNet32(nn.Module):
         out_tensor = F.adaptive_avg_pool2d(out_tensor, (1, 1))  # (B, 256, 1, 1)
         out_tensor = out_tensor.view(out_tensor.size(0), -1)  # (B, 256)
         
-        out = self.linear(out_tensor)
+
         return out
 
 # 모델 인스턴스화
