a640060062794a1b8f595bfa037cfbff65ed1b7a
18101224 <alswo01287@naver.com>
2025-08-25 09:10:27 +0900

    cifar10 ece script

--- shells/7_adaptive.sh
+++ shells/7_adaptive.sh
@@ -1,10 +1,25 @@
-LEARNING_RATES=(0.0001 0.00001 0.00003)
+#!/bin/bash
 
-for LR in "${LEARNING_RATES[@]}"; do
-CUDA_VISIBLE_DEVICES=0 python3 train_imbalanced.py --world_size=1 --learning_rate=$LR \
-                        --batch_size=128 --n_epochs=200 --weight_decay=5e-4 --scheduler=cosine --cos=True --num_workers=4 \
-                        --wandb_token=../wandb.txt --cosine_scaling=32 --loss=CE --model_type=ir50 --dataset_name=RAF-DB \
-                        --ce_weight=1 --cl_weight=0.35 --feature_branch=True --dataset_path=../data/RAF-DB --aug=True --cutout=True \
-                        --use_wandb=True
-wait
+
+make_cmd () {
+
+  local EXTRA=$1        # loss·weight·스케줄 인자 묶음
+  python3 train_imbalanced.py \
+        --batch_size=256 --n_epochs=200 --weight_decay=5e-4 \
+        --cos=True --momentum=0.9 --world_size=1 --wandb_token=../wandb.txt \
+        --model_type=e2_resnet32  --imb_type=exp --imb_factor=0.01 \
+        --dataset_path=../data --aug=True --cutout=True --use_wandb=True  --feature_branch=True  --regular_simplex=True --feature_module=deepcomplex_3 --use_tf=True \
+         --cosine_scaling=32   --temperature=0.1 --scheduler=warmup $EXTRA
+}
+
+
+for lr in 0.1 0.15; do
+
+  CUDA_VISIBLE_DEVICES=0 make_cmd  "--learning_rate=$lr --dataset_name=cifar10 --use_mean=True --loss=BCL_ECE --ece_weight=1 " &
+
+  CUDA_VISIBLE_DEVICES=1 make_cmd  "--learning_rate=$lr --dataset_name=cifar10 --use_mean=True --loss=BCL_ECE --ece_weight=0.3" &
+                          
+  CUDA_VISIBLE_DEVICES=2 make_cmd  "--learning_rate=$lr --dataset_name=cifar10 --use_mean=True --loss=BCL_ECE --ece_weight=0.5" &
+
+  wait
 done
