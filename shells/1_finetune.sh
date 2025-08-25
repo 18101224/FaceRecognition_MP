f427a9c1958541e50271f6d7a2f1b6b64e5ac5a2
18101224 <alswo01287@naver.com>
2025-08-25 09:59:09 +0900

    cifar10 resnet script

--- shells/1_finetune.sh
+++ shells/1_finetune.sh
@@ -10,24 +10,26 @@ make_cmd () {
   python3 train_imbalanced.py \
         --learning_rate="$LR" --batch_size=256 --n_epochs=200 --weight_decay=5e-4 \
         --cos=True --momentum=0.9 --world_size=1 --wandb_token=../wandb.txt \
-        --model_type=e2_resnet32 --dataset_name=cifar100 --imb_type=exp --imb_factor=0.01 \
-        --dataset_path=../data --aug=True --cutout=True --use_wandb=True  --feature_branch=True  --regular_simplex=True --feature_module=deepcomplex_3 --use_tf=True $EXTRA
+        --model_type=resnet32 --dataset_name=cifar10 --imb_type=exp --imb_factor=0.01 \
+        --dataset_path=../data --aug=True --cutout=True --use_wandb=True  --feature_branch=True --temperature=0.1 --scheduler=warmup --cosine_scaling=32 $EXTRA
 }
 
 
+make_cmd 0.15 "--loss=BCL --ce_weight=2 --cl_weight=0.6 " &
+
+make_cmd 0.1 "--loss=BCL --ce_weight=2 --cl_weight=0.6" &
+
+wait
+
 for LR in "${LEARNING_RATES[@]}"; do
 
-  # make_cmd "$LR" "--loss=BCL --ce_weight=2 --cl_weight=0.6  --cosine_scaling=32  \
-  #                         --temperature=0.1 --scheduler=warmup" 
+  make_cmd 0.15 "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.3" &
 
-  CUDA_VISIBLE_DEVICES=0 make_cmd "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.3  --cosine_scaling=32  \
-                          --temperature=0.1 --scheduler=warmup" &  
+  make_cmd 0.1 "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.5" &
 
-  CUDA_VISIBLE_DEVICES=1 make_cmd "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.5  --cosine_scaling=32  \
-                          --temperature=0.1 --scheduler=warmup" &  
-                          
-  CUDA_VISIBLE_DEVICES=2 make_cmd "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=1  --cosine_scaling=32  \
-                          --temperature=0.1 --scheduler=warmup" &  
+  make_cmd 0.1 "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=1" &
+  
+  make_cmd 0.1 "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.1" &
 
   wait
           # ---- 네 개 모두 종료될 때까지 대기 ----
--- shells/3_cifar.sh
+++ shells/3_cifar.sh
@@ -2,33 +2,32 @@
 # run_all_cifar100.sh
 
 
-LEARNING_RATES=(0.15 0.1 0.07 0.05)
+LEARNING_RATES=(0.15 0.1 )
 
 make_cmd () {
   local LR=$1
   local EXTRA=$2        # loss·weight·스케줄 인자 묶음
   python3 train_imbalanced.py \
         --learning_rate="$LR" --batch_size=256 --n_epochs=200 --weight_decay=5e-4 \
-        --cos=True --momentum=0.9 --world_size=1 --wandb_token=../wandb.txt --server=7 \
+        --cos=True --momentum=0.9 --world_size=1 --wandb_token=../wandb.txt \
         --model_type=resnet32 --dataset_name=cifar10 --imb_type=exp --imb_factor=0.01 \
-        --dataset_path=../data --aug=True --cutout=True --use_wandb=True --regular_simplex=True $EXTRA
+        --dataset_path=../data --aug=True --cutout=True --use_wandb=True  --feature_branch=True --temperature=0.1 --scheduler=warmup --cosine_scaling=32 $EXTRA
 }
+
+
 
 
 for LR in "${LEARNING_RATES[@]}"; do
 
-  make_cmd "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.1  --cosine_scaling=32 --feature_module=deepcomplex_3 \
-                          --temperature=0.1 --scheduler=warmup"  &
+  make_cmd 0.15 "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.3 --use_mean=True" &
 
-  make_cmd "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6   --ece_weight=0.05 --cosine_scaling=32 --feature_module=deepcomplex_3 \
-                          --temperature=0.1 --scheduler=warmup"  &
+  make_cmd 0.1 "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.5 --use_mean=True" &
 
-  make_cmd "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6   --ece_weight=0.03 --cosine_scaling=32 --feature_module=deepcomplex_3 \
-                          --temperature=0.1 --scheduler=warmup"  &
+  make_cmd 0.1 "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=1 --use_mean=True" &
+  
+  make_cmd 0.1 "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.1 --use_mean=True" &
 
-  make_cmd "$LR" "--loss=BCL_ECE --ce_weight=2 --cl_weight=0.6   --ece_weight=0.07 --cosine_scaling=32 --feature_module=deepcomplex_3 \
-                          --temperature=0.1 --scheduler=warmup"  &
-
-  wait          # ---- 네 개 모두 종료될 때까지 대기 ----
+  wait
+          # ---- 네 개 모두 종료될 때까지 대기 ----
 done
 
