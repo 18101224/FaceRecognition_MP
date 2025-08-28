3e9af37b7e33314a8078367a11bfafc59a2d1050
18101224 <alswo01287@naver.com>
2025-08-28 18:00:02 +0900

    pm imnagenet shell

--- shells/pm_imgnet.sh
+++ shells/pm_imgnet.sh
@@ -31,7 +31,7 @@ make_cmd () {
 
   local EXTRA=$1        # loss·weight·스케줄 인자 묶음
   python3 train_imbalanced.py \
-        --batch_size=256 --n_epochs=200 --weight_decay=5e-4 \
+        --batch_size=256 --n_epochs=90 --weight_decay=5e-4 \
           --cos=True --momentum=0.9 --world_size=1 \
         --model_type=resnext50  --imb_type=exp --imb_factor=0.01 \
         --dataset_path=../data/imgnet --aug=True --cutout=True --use_wandb=True  --feature_branch=True --use_tf=True \
