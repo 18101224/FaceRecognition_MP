f5a413bb2cf417ffd67a4188938e80620f877fc1
18101224 <alswo01287@naver.com>
2025-08-31 17:29:46 +0900

    perlmutter shared

--- shells/pm_cifar.sh
+++ shells/pm_cifar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J hcir_cifar
 #SBATCH -A m1248_g 
-#SBATCH -q debug
+#SBATCH -q shared
 #SBATCH -N 1
-#SBATCH -t 00:05:00
+#SBATCH -t 48:00:00
 #SBATCH --gpus-per-node=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
