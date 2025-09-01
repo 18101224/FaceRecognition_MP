63d2f96a3ae2446d2e27be6541ae603a35af1357
18101224 <alswo01287@naver.com>
2025-09-01 12:58:39 +0900

    baseline script and uuid

--- shells/pm_imgnet.sh
+++ shells/pm_imgnet.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J hcir_imgnet
 #SBATCH -A m1248_g 
-#SBATCH -q regular
+#SBATCH -q debug
 #SBATCH -N 1
-#SBATCH -t 48:00:00
+#SBATCH -t 00:20:00
 #SBATCH --gpus-per-node=4
 #SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=16
