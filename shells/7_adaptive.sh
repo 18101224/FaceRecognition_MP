7875f41cc82fa465a7343af6365b14f6a9f92eed
18101224 <alswo01287@naver.com>
2025-09-03 15:48:37 +0900

    ir50 pretrained weight fixed

--- shells/7_adaptive.sh
+++ shells/7_adaptive.sh
@@ -1,47 +1,15 @@
 #!/bin/bash
-
-
-make_cmd () {
-
-  local EXTRA=$1        # loss·weight·스케줄 인자 묶음
-  python3 train_imbalanced.py \
-        --batch_size=256 --n_epochs=200 --weight_decay=5e-4 \
-        --cos=True --momentum=0.9 --world_size=1  \
-        --model_type=resnet32  --imb_type=exp --imb_factor=0.01 \
-        --dataset_path=../data --aug=True --cutout=True --use_wandb=True  --feature_branch=True  --use_tf=True \
-         --cosine_scaling=32 --temperature=0.1 --scheduler=warmup --num_workers=16 $EXTRA
+make_cmd(){
+  param1=$1 
+  CUDA_VISIBLE_DEVICES=2 python3 train_imbalanced.py --world_size=1 \
+      --learning_rate=0.15 --batch_size=256 --n_epochs=200 --scheduler=warmup --weight_decay=5e-4 \
+      --warmup_epochs=5 --cos=True --use_tf=True --num_workers=16 --use_wandb=True \
+      --cosine_scaling=32 --loss=BCL_ECE --ce_weight=1 --cl_weight=1  --model_type=resnet32 --feature_branch=True \
+      --dataset_name=cifar10 --dataset_path=../data --imb_factor=0.01 --aug=True --cutout=True $param1
 }
 
-gpu0 (){
-for dataset in cifar100 cifar10 ; do
-  for lr in 0.1 0.15; do
 
-    CUDA_VISIBLE_DEVICES=0 make_cmd  "--learning_rate=$lr --dataset_name=$dataset --use_mean=True --loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=1 --ece_scheduling=cosine" &
-
-    CUDA_VISIBLE_DEVICES=0 make_cmd  "--learning_rate=$lr --dataset_name=$dataset --use_mean=True --loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.3 --ece_scheduling=cosine" &
-                            
-    CUDA_VISIBLE_DEVICES=0 make_cmd  "--learning_rate=$lr --dataset_name=$dataset --use_mean=True --loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.5 --ece_scheduling=cosine" &
-
-    wait
-  done
-  wait
-done 
-}
-
-gpu1 (){
-  for dataset in cifar100 cifar10 ; do
-    for lr in 0.1 0.15; do
-      CUDA_VISIBLE_DEVICES=2 make_cmd  "--learning_rate=$lr --dataset_name=$dataset  --loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.3  " &
-
-      CUDA_VISIBLE_DEVICES=2 make_cmd  "--learning_rate=$lr --dataset_name=$dataset  --loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.5  " &
-
-      CUDA_VISIBLE_DEVICES=2 make_cmd  "--learning_rate=$lr --dataset_name=$dataset  --loss=BCL_ECE --ce_weight=2 --cl_weight=0.6 --ece_weight=0.7  " &
-    wait 
-    done
-    wait
-  done
-}
-
-gpu0 &
-gpu1 &
+make_cmd '--ece_weight=0.3' &
+make_cmd '--ece_weight=0.5' &
+make_cmd '--ece_weight=1' &
 wait
