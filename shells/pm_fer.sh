8c5bdfb91741994b9461c38f11f8f70a4ad7c45b
18101224 <alswo01287@naver.com>
2025-09-04 15:22:40 +0900

    FER kprpe added

--- shells/pm_fer.sh
+++ shells/pm_fer.sh
@@ -4,7 +4,7 @@
 #SBATCH -q debug
 #SBATCH -N 1
 #SBATCH -t 00:30:00
-#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-node=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
 #SBATCH --output=slurm-%x-%j.out
