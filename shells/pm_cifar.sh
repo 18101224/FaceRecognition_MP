5350da19a263479c2d8b58d70800cf7e446109be
18101224 <alswo01287@naver.com>
2025-09-05 18:35:52 +0900

    pm script

--- shells/pm_cifar.sh
+++ shells/pm_cifar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J hcir_cifar
 #SBATCH -A m1248_g 
-#SBATCH -q debug
+#SBATCH -q regular
 #SBATCH -N 1
-#SBATCH -t 0:10:00
+#SBATCH -t 10:00:00
 #SBATCH --gpus-per-node=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
