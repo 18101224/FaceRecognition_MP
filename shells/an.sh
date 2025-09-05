3b21c56341b6de4ceff7d8cf9c5dd26228ce508d
18101224 <alswo01287@naver.com>
2025-09-05 16:15:14 +0900

    analysis

--- shells/an.sh
+++ shells/an.sh
@@ -1,3 +1,3 @@
 python3 analyze_classifier.py --dataset_name=RAF-DB \
  --model_paths checkpoint/07db3fe34-a755-44e1-9d37-cffc175f2d71 --save_path="results/RAF-DB_CE_IR50_WO_sampler" \
- --dataset_path=../data/RAF-DB_balanced --mode=analysis --ckpt_type=best_acc --model_type=ir50 --cos=True
+ --dataset_path=../data/RAF-DB_balanced --mode=analysis --ckpt_type=best_acc --model_type=ir50 
