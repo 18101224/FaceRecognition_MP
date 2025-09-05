99a9d3d17096767f33f744d17b35dc35da1f051b
18101224 <alswo01287@naver.com>
2025-09-05 18:41:20 +0900

    pm script

--- shells/7_fer.sh
+++ shells/7_fer.sh
@@ -5,10 +5,13 @@ make_cmd () {
   python3 train_imbalanced.py \
         --batch_size=128 --n_epochs=200 --weight_decay=5e-4 \
          --momentum=0.9 --world_size=1 \
-         --imb_type=exp --imb_factor=0.01 \
+         --imb_type=exp --imb_factor=0.01 --cos=True \
         --dataset_path=../data/RAF-DB_balanced --aug=True --cutout=True --use_wandb=True --use_sampler=True --feature_branch=True --use_tf=True --num_workers=16 \
-         --cosine_scaling=4    --temperature=0.1 --cl_weight=1 --ce_weight=1 $EXTRA
+         --cosine_scaling=4 --temperature=0.1 --cl_weight=1 --ce_weight=1 $EXTRA
 }
 
 #without cos 
-CUDA_VISIBLE_DEVICES=2 make_cmd '--learning_rate=9e-6 --dataset_name=RAF-DB --loss=CE --model_type=ir50' 
+for weight in 0.3 0.7 ; do
+CUDA_VISIBLE_DEVICES=2 make_cmd '--learning_rate=9e-6 --dataset_name=RAF-DB --loss=BCL --model_type=ir50 --cl_weight=0.35 --ce_weight=1' 
+done
+wait
