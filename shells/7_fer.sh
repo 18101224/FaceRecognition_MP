4e9bcbb674d2b03ef50e42b4b00fcae45a0880b1
18101224 <alswo01287@naver.com>
2025-09-05 09:25:59 +0900

    raf-db balanced script

---
+++ shells/7_fer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+make_cmd () {
+
+  local EXTRA=$1        # loss·weight·스케줄 인자 묶음
+  python3 train_imbalanced.py \
+        --batch_size=128 --n_epochs=200 --weight_decay=5e-4 \
+          --cos=True --momentum=0.9 --world_size=1 \
+         --imb_type=exp --imb_factor=0.01 \
+        --dataset_path=../data --aug=True --cutout=True --use_wandb=True  --feature_branch=True --use_tf=True --num_workers=16 \
+         --cosine_scaling=4    --temperature=0.1 --cl_weight=1 --ce_weight=1 $EXTRA
+}
+
+CUDA_VISIBLE_DEVICES=0 make_cmd '--learning_rate=9e-6--dataset_name=RAF-DB --loss=CE--model_type=ir50' &
