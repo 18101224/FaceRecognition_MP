b738c5dd683ca38bfb9ac307abedd86620473c7d
18101224 <alswo01287@naver.com>
2025-09-05 16:29:06 +0900

    analysis

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -33,6 +33,7 @@ class Analysis:
         self.model = get_model(self.args)
         self.model.load_state_dict(torch.load(model_ckpt, map_location=self.device,weights_only=False)['model_state_dict'])
         self.model = self.model.to(self.device)
+        self.model.eval()
         self.aligner = load_aligner(self.args.aligner_path) if self.args.aligner_path else None
         # self.backbone_analysis = Analyze_backbone(self.args)
         self.backbone = self.model.backbone 
