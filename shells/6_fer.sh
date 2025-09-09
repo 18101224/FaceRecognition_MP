697c389df86c95c63a1e4c9a21123e45b4c6b69d
18101224 <alswo01287@naver.com>
2025-09-09 13:57:44 +0900

    hcm exp id

--- shells/6_fer.sh
+++ shells/6_fer.sh
@@ -1,13 +1,11 @@
 
-make_cmd () {
-
-  local EXTRA=$1        # loss·weight·스케줄 인자 묶음
-  CUDA_VISIBLE_DEVICES=0,2 /home/gpuadmin/anaconda3/envs/main/bin/torchrun --nproc_per_node=2 train_imbalanced.py \
-        --batch_size= --n_epochs=200 --weight_decay=5e-4 \
-          --cos=True --momentum=0.9 --world_size=2 \
-        --model_type=ir50  --imb_type=exp --imb_factor=0.01 \
-        --dataset_path=../data/RAF-DB_aligned --dataset_name=RAF-DB --use_sampler=True --aug=True --cutout=True --use_wandb=True  --feature_branch=True --use_tf=True --num_workers=32 \
-         --cosine_scaling=1  --temperature=0.1 --scheduler=warmup  $EXTRA
-}
-
-make_cmd "--learning_rate=3e-5 --loss=BCL --cl_weight=1 --ce_weight=1" 
+CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node=2 QCS_hcm.py--world_size=2  \
+  --dataset_path=../data/RAF-DB_balanced \
+  --dataset_name=RAF-DB \
+  --guide_path=checkpoint/pme3c5fe43-a9ec-4a37-8ea4-f11268a0ec6e \
+  --num_workers=32 \
+  --batch_size=128 \
+  --n_epochs=200 \
+  --learning_rate=1e-5 \
+  --use_sampler=True \
+  --model_type=kp_rpe --loss=HCM --cl_weight=0.3 
