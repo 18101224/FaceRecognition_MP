7b3603958cae7dd061bec3dcea7804f58ed4a972
18101224 <alswo01287@naver.com>
2025-09-22 08:07:55 +0900

    memory monitoring

---
+++ Loss/CL/ETF.py
@@ -0,0 +1,17 @@
+import torch 
+
+__all__ = ['compute_etf_loss']
+
+def compute_etf_loss(weight, etf_weight, statistics=False):
+    '''
+    weight : num_classes, dim, normalized weight
+    '''
+    sims = weight @ weight.T 
+    i, j = torch.triu_indices(weight.shape[0], weight.shape[0], offset=1)
+
+    if not statistics:
+        rho = -1/(sims.shape[0]-1)
+        loss = ((sims[i,j].reshape(-1)-rho)**2).mean()
+    else:
+        loss = sims[i,j].reshape(-1).mean() + sims[i,j].reshape(-1).std() * 0.7 
+    return loss*etf_weight
