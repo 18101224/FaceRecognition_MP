db5dc0d7abfc4e8e286fa254e4a2f66f0beee30e
18101224 <alswo01287@naver.com>
2025-09-22 08:05:49 +0900

    memory monitoring

--- shells/pm_fer.sh
+++ shells/pm_fer.sh
@@ -24,7 +24,7 @@ conda activate /pscratch/sd/s/sgkim/hcir/cv
 # NODELIST=$(scontrol show hostnames "$SLURM_NODELIST")
 # NODE1=$(echo "$NODELIST" | sed -n '1p')
 
-python3 Moco.py --world_size=1 --num_workers=32 --use_tf=True \
+python3 MoCo.py --world_size=1 --num_workers=32 --use_tf=True \
 --learning_rate=1e-5 --batch_size=256 --n_epochs=200 --weight_decay=5e-4 \
 --dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7 --use_sampler=True --img_size=112 \
 --mean_weight=checkpoint/kprpe12m_72K \
