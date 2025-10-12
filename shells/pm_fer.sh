7a233d65cb34d1e3b5f314d5e20d8c8fba2c4d74
18101224 <alswo01287@naver.com>
2025-10-12 17:43:32 +0900

    perlmutter ce script

--- shells/pm_fer.sh
+++ shells/pm_fer.sh
@@ -47,3 +47,5 @@ CUDA_VISIBLE_DEVICES=3 python3 FER_CL.py --world_size=1 --num_workers=32 --use_t
 --dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7 --img_size=112 \
 --model_type=kprpe12m \
 --loss=CE &
+
+wait
