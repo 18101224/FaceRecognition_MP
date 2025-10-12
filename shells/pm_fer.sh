52c0618a4cda173910a193a8d5cbb4e91c8cb4d5
18101224 <alswo01287@naver.com>
2025-10-12 15:25:53 +0900

    perlmutter ce script

--- shells/pm_fer.sh
+++ shells/pm_fer.sh
@@ -28,22 +28,22 @@ CUDA_VISIBLE_DEVICES=0 python3 FER_CL.py --world_size=1 --num_workers=32 --use_t
 --learning_rate=1e-5 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
 --dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7 --use_sampler=True --img_size=112 \
 --model_type=kprpe12m \
---loss=CE
+--loss=CE & 
 
 CUDA_VISIBLE_DEVICES=1 python3 FER_CL.py --world_size=1 --num_workers=32 --use_tf=True \
 --learning_rate=1e-5 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
 --dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7 --use_sampler=True --img_size=112 \
 --model_type=kprpe12m \
---loss=CE
+--loss=CE &
 
 CUDA_VISIBLE_DEVICES=2 python3 FER_CL.py --world_size=1 --num_workers=32 --use_tf=True \
 --learning_rate=1e-5 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
 --dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7 --img_size=112 \
 --model_type=kprpe12m \
---loss=CE
+--loss=CE &
 
 CUDA_VISIBLE_DEVICES=3 python3 FER_CL.py --world_size=1 --num_workers=32 --use_tf=True \
 --learning_rate=1e-5 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
 --dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7 --img_size=112 \
 --model_type=kprpe12m \
---loss=CE
+--loss=CE &
