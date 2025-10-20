3ffd8a3d5cba6aa22eb6e491a15f97a515ea1ca1
18101224 <alswo01287@naver.com>
2025-10-20 10:37:34 +0900

    perlmutter script

--- shells/pm_fer.sh
+++ shells/pm_fer.sh
@@ -26,8 +26,9 @@ conda activate /pscratch/sd/s/sgkim/hcir/cv
 
 
 python3 FER_CL.py --world_size=1 --num_workers=32 --use_tf=True \
---learning_rate=1e-6 --batch_size=144 --n_epochs=30 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
+--learning_rate=1e-6 --batch_size=256 --n_epochs=30 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
 --dataset_name=AffectNet --dataset_path=../data/AffectNet7 --num_classes=7 --use_sampler=True --img_size=112 \
 --model_type=kprpe12m --feature_branch=True --use_bn=True \
 --loss=EKCL --num_clusters 80 200 --sizes_clusters 3 5 --balanced_cl=True --batch_pairs_only=True --utilize_target_centers=True 
-wait 
+
+pythn3 -c "from utils.pushover import send_message; send_message(' pm_fer finished')"
