3a95ecae2a7bd3914c058515ffa6fced8d3cce64
18101224 <alswo01287@naver.com>
2025-10-25 11:31:50 +0900

    perlmutter script

--- shells/pm_fer.sh
+++ shells/pm_fer.sh
@@ -27,7 +27,7 @@ conda activate /pscratch/sd/s/sgkim/hcir/cv
 
 python3 FER_CL.py --world_size=1 --num_workers=32 --use_tf=True \
 --learning_rate=1e-5 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
---dataset_name=AffectNet --dataset_path=../data/AffectNet7 --num_classes=7 --use_sampler=True --img_size=112 \
+--dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7 --use_sampler=True --img_size=112 \
 --model_type=kprpe12m --feature_branch=True --use_bn=True \
 --loss=KBCL_ETF --kcl_k=10 --moco_k=256 --utilize_target_centers=True  --etf_weight=1 --balanced_cl=True --temperature=0.1 
 
