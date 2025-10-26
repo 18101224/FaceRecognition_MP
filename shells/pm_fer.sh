5dcc69e596baead738bf2d3bb276063364b9ffc1
18101224 <alswo01287@naver.com>
2025-10-27 08:52:38 +0900

    perlmutter scirpt

--- shells/pm_fer.sh
+++ shells/pm_fer.sh
@@ -29,25 +29,25 @@ CUDA_VISIBLE_DEVICES=0 python3 FER_CL.py --world_size=1 --num_workers=32 --use_t
 --learning_rate=1e-6 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
 --dataset_name=AffectNet --dataset_path=../data/AffectNet8 --num_classes=8 --use_sampler=True --img_size=112 \
 --model_type=kprpe12m --feature_branch=True --use_bn=True \
---loss=KBCL_ETF --kcl_k=10 --moco_k=1024 --utilize_target_centers=True --etf_weight=1.5 --balanced_cl=True --temperature=0.1 &
+--loss=KBCL_ETF --kcl_k=32 --moco_k=1024 --utilize_target_centers=True --etf_weight=1.5 --balanced_cl=True --temperature=0.1 &
 
 CUDA_VISIBLE_DEVICES=1 python3 FER_CL.py --world_size=1 --num_workers=32 --use_tf=True \
 --learning_rate=1e-6 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
 --dataset_name=AffectNet --dataset_path=../data/AffectNet8 --num_classes=8 --use_sampler=True --img_size=112 \
 --model_type=kprpe12m --feature_branch=True --use_bn=True \
---loss=KBCL_ETF --kcl_k=10 --moco_k=1024 --utilize_target_centers=True --etf_weight=1.5 --balanced_cl=True --temperature=0.1 &
+--loss=KBCL_ETF --kcl_k=32 --moco_k=1024 --utilize_target_centers=True --etf_weight=1.5 --balanced_cl=True --temperature=0.1 &
 
 CUDA_VISIBLE_DEVICES=2 python3 FER_CL.py --world_size=1 --num_workers=32 --use_tf=True \
 --learning_rate=1e-6 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
 --dataset_name=AffectNet --dataset_path=../data/AffectNet8 --num_classes=8 --use_sampler=True --img_size=112 \
 --model_type=kprpe12m --feature_branch=True --use_bn=True \
---loss=KBCL_ETF --kcl_k=10 --moco_k=1024 --utilize_target_centers=True --etf_weight=1.5 --balanced_cl=True --temperature=0.1 &
+--loss=KBCL_ETF --kcl_k=32 --moco_k=1024 --utilize_target_centers=True --etf_weight=1.5 --balanced_cl=True --temperature=0.1 &
 
 CUDA_VISIBLE_DEVICES=3 python3 FER_CL.py --world_size=1 --num_workers=32 --use_tf=True \
 --learning_rate=1e-6 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
 --dataset_name=AffectNet --dataset_path=../data/AffectNet8 --num_classes=8 --use_sampler=True --img_size=112 \
 --model_type=kprpe12m --feature_branch=True --use_bn=True \
---loss=KBCL_ETF --kcl_k=10 --moco_k=1024 --utilize_target_centers=True --etf_weight=1.5 --balanced_cl=True --temperature=0.1 &
+--loss=KBCL_ETF --kcl_k=32 --moco_k=1024 --utilize_target_centers=True --etf_weight=1.5 --balanced_cl=True --temperature=0.1 &
 
 wait
 
