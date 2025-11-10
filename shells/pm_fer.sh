1897fa5b0b456934d5c7365dc872da220d9e26d8
18101224 <alswo01287@naver.com>
2025-11-10 20:57:06 +0900

    perlmutter script

--- shells/pm_fer.sh
+++ shells/pm_fer.sh
@@ -28,15 +28,15 @@ conda activate /pscratch/sd/s/sgkim/hcir/cv
 
 CUDA_VISIBLE_DEVICES=0 python3 FER_CL.py --world_size=1 --num_workers=32 --use_tf=True \
 --learning_rate=1e-5 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
---dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7 --use_sampler=True --img_size=112 \
+--dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7  --img_size=112 \
 --model_type=kprpe12m --feature_branch=True --use_bn=True \
---loss=KBCL_ETF --etf_weight=0.3 --beta=0.3 --utilize_target_centers=True --moco_k=1024 --temperature=0.1 --kcl_k=10 &
+--loss=KBCL_ETF --etf_weight=0.05 --beta=0.3 --utilize_target_centers=True --moco_k=1024 --temperature=0.1 --kcl_k=10 &
 
 CUDA_VISIBLE_DEVICES=1 python3 FER_CL.py --world_size=1 --num_workers=32 --use_tf=True \
 --learning_rate=1e-5 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
---dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7 --use_sampler=True --img_size=112 \
+--dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7  --img_size=112 \
 --model_type=kprpe12m --feature_branch=True --use_bn=True \
---loss=KBCL_ETF --etf_weight=0.6 --beta=0.3 --utilize_target_centers=True --moco_k=1024 --temperature=0.1 --kcl_k=10 &
+--loss=KBCL_ETF --etf_weight=0.1 --beta=0.3 --utilize_target_centers=True --moco_k=1024 --temperature=0.1 --kcl_k=10 &
 wait
 
 python3 -c "from utils.pushover import send_message; send_message('pm_fer finished')"
