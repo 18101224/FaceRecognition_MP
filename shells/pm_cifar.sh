d529e29a0c9774cd6ed4f95d1a236eff3ba1222a
18101224 <alswo01287@naver.com>
2025-11-17 19:17:31 +0900

    perlmutter script

--- shells/pm_cifar.sh
+++ shells/pm_cifar.sh
@@ -16,7 +16,7 @@
 source /pscratch/sd/s/sgkim/hcir/mc/bin/activate
 conda activate /pscratch/sd/s/sgkim/hcir/cv 
 
-torchrun --nproc_per_node=4 FER_CL.py -world_size=4 --num_workers=32 \
+torchrun --nproc_per_node=4 FER_CL.py --world_size=4 --num_workers=32 \
 --learning_rate=1e-6 --batch_size=256 --n_epochs=30 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
 --dataset_name=AffectNet --dataset_path=../data/AffectNet7 --num_classes=7 --use_sampler=True --img_size=112 \
 --model_type=kprpe12m --resume_path=checkpoint/pmf9467999-0e31-47e1-a172-a41704c4b465 \
