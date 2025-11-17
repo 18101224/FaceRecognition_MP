b97394f7fcd0b84fb7c1ef65f418bbb3c49207f9
18101224 <alswo01287@naver.com>
2025-11-17 18:38:43 +0900

    perlmutter script

--- shells/pm_cifar.sh
+++ shells/pm_cifar.sh
@@ -3,9 +3,9 @@
 #SBATCH -A m1248_g 
 #SBATCH -q regular
 #SBATCH -N 1
-#SBATCH -t 12:00:00
+#SBATCH -t 17:00:00
 #SBATCH --gpus-per-node=4
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
 #SBATCH --output=slurm-%x-%j.out
 #SBATCH --error=slurm-%x-%j.err
@@ -16,26 +16,8 @@
 source /pscratch/sd/s/sgkim/hcir/mc/bin/activate
 conda activate /pscratch/sd/s/sgkim/hcir/cv 
 
-
-export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
-export MKL_NUM_THREADS=${OMP_NUM_THREADS}
-export OPENBLAS_NUM_THREADS=${OMP_NUM_THREADS}
-
-# inside pm_cifar.sh (replace the 4 separate srun blocks)
-
-srun -n 4 -c ${SLURM_CPUS_PER_TASK} \
-  --gpus-per-task=1 --gpu-bind=map_gpu:0,1,2,3 --cpu-bind=cores \
-  --kill-on-bad-exit=1 -l \
-  bash -lc 'i=$SLURM_LOCALID; \
-    case "$i" in \
-      0) bs=64 ;; \
-      1) bs=128 ;; \
-      2) bs=32 ;; \
-      3) bs=256 ;; \
-      *) echo "Unsupported SLURM_LOCALID: $i" >&2; exit 1 ;; \
-    esac; \
-    python3 MoCo.py --learning_rate=1e-5 --batch_size="$bs" --n_epochs=200 \
-    --world_size=1 --num_workers="${SLURM_CPUS_PER_TASK}" --use_tf=True --weight_decay=5e-4 \
-    --dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7 --use_sampler=True \
-    --mean_weight=checkpoint/kprpe_72K_${bs} --model_type=kprpe12m \
-    --loss=KBCL --kcl_k=5 --beta=0.3 --temperature=0.1 --utilze_class_centers=True --moco_k=72'
+torchrun --nproc_per_node=4 FER_CL.py -world_size=4 --num_workers=32 \
+--learning_rate=1e-6 --batch_size=256 --n_epochs=30 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
+--dataset_name=AffectNet --dataset_path=../data/AffectNet7 --num_classes=7 --use_sampler=True --img_size=112 \
+--model_type=kprpe12m --resume_path=checkpoint/pmf9467999-0e31-47e1-a172-a41704c4b465 \
+--loss=BEAC --beta=2 --utilize_target_centers=True
