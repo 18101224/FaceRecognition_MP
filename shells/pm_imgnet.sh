c0b338c2ae6735273406daa11e18bf95ca42946e
18101224 <alswo01287@naver.com>
2025-11-18 10:25:56 +0900

    perlmutter script

--- shells/pm_imgnet.sh
+++ shells/pm_imgnet.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH -J hcir_fer
 #SBATCH -A m1248_g 
-#SBATCH -q regular
+#SBATCH -q shared
 #SBATCH -N 1
-#SBATCH -t 14:00:00
-#SBATCH --gpus-per-node=4
+#SBATCH -t 07:00:00
+#SBATCH --gpus-per-node=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=128
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=32
 #SBATCH --output=slurm-%x-%j.out
 #SBATCH --error=slurm-%x-%j.err
 #SBATCH --mail-user=alswo01287@naver.com
@@ -24,8 +25,9 @@ conda activate /pscratch/sd/s/sgkim/hcir/cv
 # NODELIST=$(scontrol show hostnames "$SLURM_NODELIST")
 # NODE1=$(echo "$NODELIST" | sed -n '1p')
 
-torchrun --nproc_per_node=2 MoCo.py --world_size=2 --num_workers=32 --use_tf=True \
---learning_rate=1e-6 --batch_size=32 --n_epochs=200 --weight_decay=5e-4 --optimizer=SAM --scheduler=exp \
---dataset_name=AffectNet --dataset_path=../data/AffectNet7 --num_classes=7 --use_sampler=True --img_size=112 --use_view=True \
---model_type=kprpe12m \
---loss=BCL --utilze_target_centers=True
+
+python3 FER_CL.py --num_workers=32 --use_tf=True \
+--learning_rate=1e-5 --batch_size=256 --n_epochs=100 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
+--dataset_name=CAER --dataset_path=../data/CAER-S --num_classes=7 --use_sampler=True --img_size=112 \
+--model_type=kprpe12m  \
+--loss=CE 
