901f884b8f090f75d7e10ab340602dc1233b0707
18101224 <alswo01287@naver.com>
2025-11-21 10:26:27 +0900

    perlmutter script

--- shells/pm_fer.sh
+++ shells/pm_fer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -J hcir_fer
 #SBATCH -A m1248_g 
-#SBATCH -q shared
+#SBATCH -q regular
 #SBATCH -N 1
 #SBATCH -t 13:00:00
-#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-node=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=32
@@ -25,10 +25,14 @@ conda activate /pscratch/sd/s/sgkim/hcir/cv
 # NODELIST=$(scontrol show hostnames "$SLURM_NODELIST")
 # NODE1=$(echo "$NODELIST" | sed -n '1p')
 
-python3 FER_CL.py --num_workers=32 --use_tf=True \
+betas=(0.1 0.3 1 2)
+for i in 0 1 2 3; do
+CUDA_VISIBLE_DEVICES=$i python3 FER_CL.py --num_workers=32 --use_tf=True \
 --learning_rate=1e-5 --batch_size=256 --n_epochs=100 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
 --dataset_name=CAER --dataset_path=../data/CAER-S --num_classes=7 --use_sampler=True --img_size=112 \
 --model_type=kprpe12m --feature_branch=True --use_bn=True \
---loss=KBCL --temperature=0.1 --utilize_target_centers=True --beta=1 --moco_k=256 --kcl_k=32 
+--loss=KBCL_ETF --etf_weight=${betas[$i]} --temperature=0.1 --utilize_target_centers=True --beta=1 --moco_k=256 --kcl_k=32 &
+done; 
 
+wait 
 python3 -c "from utils.pushover import send_message; send_message('pm_fer finished')"
