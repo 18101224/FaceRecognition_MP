adc4c5f3e3956c2a27a44bd6a1e488fb46303a0c
18101224 <alswo01287@naver.com>
2025-11-22 17:21:00 +0900

    perlmutter script beac raf-db

--- shells/7_fer.sh
+++ shells/7_fer.sh
@@ -1,8 +1,3 @@
-CUDA_VISIBLE_DEVICES=0,2 torchrun --nproc_per_node=2 FER_CL.py --world_size=2 --num_workers=32 --use_tf=True \
---learning_rate=1e-5 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
---dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7 --use_sampler=True --img_size=112 \
---model_type=kprpe12m  \
---loss=BEAC --beta=2 --utilize_target_centers=True
 
 CUDA_VISIBLE_DEVICES=0,2 torchrun --nproc_per_node=2 FER_CL.py --world_size=2 --num_workers=32 --use_tf=True \
 --learning_rate=1e-5 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
--- shells/pm_fer.sh
+++ shells/pm_fer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH -J hcir_fer
 #SBATCH -A m1248_g 
-#SBATCH -q shared
+#SBATCH -q regular
 #SBATCH -N 1
-#SBATCH -t 31:00:00
-#SBATCH --gpus-per-node=1
+#SBATCH -t 11:00:00
+#SBATCH --gpus-per-node=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=128
 #SBATCH --output=slurm-%x-%j.out
 #SBATCH --error=slurm-%x-%j.err
 #SBATCH --mail-user=alswo01287@naver.com
@@ -25,11 +25,11 @@ conda activate /pscratch/sd/s/sgkim/hcir/cv
 # NODELIST=$(scontrol show hostnames "$SLURM_NODELIST")
 # NODE1=$(echo "$NODELIST" | sed -n '1p')
 
-python3 FER_CL.py --num_workers=32 --use_tf=True \
+torchrun --nproc_per_node=4 FER_CL.py --world_size=4 --num_workers=128 --use_tf=True \
 --learning_rate=1e-6 --batch_size=256 --n_epochs=30 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
 --dataset_name=AffectNet --dataset_path=../data/AffectNet8 --num_classes=8 --use_sampler=True --img_size=112 \
 --model_type=kprpe12m --feature_branch=True --use_bn=True \
---loss=KBCL --temperature=0.1 --utilize_target_centers=True --beta=1 --moco_k=1024 --kcl_k=32 
+--loss=BEAC --utilize_target_centers=True --beta=2  
 
 
 wait 
