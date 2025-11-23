2255ce4ae53009fdf3146d48d8816983b4809120
18101224 <alswo01287@naver.com>
2025-11-23 10:44:06 +0900

    perlmutter raf-db script

--- shells/pm_imgnet.sh
+++ shells/pm_imgnet.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -J hcir_fer
 #SBATCH -A m1248_g 
-#SBATCH -q shared
+#SBATCH -q regular
 #SBATCH -N 1
-#SBATCH -t 07:00:00
-#SBATCH --gpus-per-node=1
-#SBATCH --ntasks-per-node=1
+#SBATCH -t 11:00:00
+#SBATCH --gpus-per-node=4
+#SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=32
 #SBATCH --output=slurm-%x-%j.out
@@ -25,9 +25,13 @@ conda activate /pscratch/sd/s/sgkim/hcir/cv
 # NODELIST=$(scontrol show hostnames "$SLURM_NODELIST")
 # NODE1=$(echo "$NODELIST" | sed -n '1p')
 
-
-python3 FER_CL.py --num_workers=32 --use_tf=True \
+ks=(5 10 32 128)
+for i in 0 1 2 3; do
+CUDA_VISIBLE_DEVICES=$i python3 FER_CL.py --world_size=1 --num_workers=32 --use_tf=True \
 --learning_rate=1e-5 --batch_size=256 --n_epochs=200 --weight_decay=1e-4 --optimizer=SAM --scheduler=exp \
 --dataset_name=RAF-DB --dataset_path=../data/RAF-DB_balanced --num_classes=7 --use_sampler=True --img_size=112 \
 --model_type=kprpe12m --feature_branch=True --use_bn=True \
---loss=SCL --beta=0.3 --temperature=0.1 
+--loss=KBCL --beta=1 --kcl_k=${ks[$i]} --moco_k=1024 --utilize_target_centers=True &
+done
+
+wait 
